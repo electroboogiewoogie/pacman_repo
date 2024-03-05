67018b77855c41ba01d8dc1be629244ab768649a
electroboogiewoogie <belov150407vb@yandex.ru>
2024-03-05 16:27:29 +0500

    добавлена загрузка уровня, пакмен реагирует на стены

--- platforms.py
+++ platforms.py
@@ -4,8 +4,24 @@ import sys
 import pygame
 
 TIMER_EVENT_TYPE = 30
-WINDOW_WIDTH, WINDOW_HEIGHT = 600, 600
+WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 600
 size = WINDOW_WIDTH, WINDOW_HEIGHT
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                continue
+                # Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                # Tile('empty', x, y)
+                new_player = Pacman(x * tile_width, y * tile_height)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
 
 
 def load_level(filename):
@@ -32,31 +48,34 @@ def load_image(name):
 
 
 tile_images = {
-    'wall': pygame.transform.scale(load_image('box.png'), (70, 70)),
-    'empty': pygame.transform.scale(load_image('grass.png'), (70, 70))
+    'wall': pygame.transform.scale(load_image('wholelottared.png'), (30, 30))
+    # 'empty': pygame.transform.scale(load_image('grass.png'), (25, 25))
 }
-player_image = pygame.transform.scale(load_image('pacman_1.png'), (50, 50))
+player_image = pygame.transform.scale(load_image('pacman_1.png'), (20, 20))
 
-tile_width = tile_height = 70
-p_width = p_height = 50
+tile_width = tile_height = 30
+p_width = p_height = 20
 
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
+        # self.image = pygame.Surface([x2, y2])
+        # self.image.fill((125, 125, 125))
+        # self.rect = pygame.Rect(x1, y1, x2, y2)
         self.image = tile_images[tile_type]
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
 
 
 class Pacman(pygame.sprite.Sprite):
-    image_1 = pygame.transform.scale(load_image('pacman_1.png'), (50, 50))
-    image_eat = pygame.transform.scale(load_image('hungry_pacman.png'), (50, 50))
-    image_fed = pygame.transform.scale(load_image('fed_pacman.png'), (50, 50))
-    image_2 = pygame.transform.scale(load_image('pacman_1.png'), (50, 50))
+    image_1 = pygame.transform.scale(load_image('pacman_1.png'), (20, 20))
+    image_eat = pygame.transform.scale(load_image('hungry_pacman.png'), (20, 20))
+    image_fed = pygame.transform.scale(load_image('fed_pacman.png'), (20, 20))
+    image_2 = pygame.transform.scale(load_image('pacman_1.png'), (20, 20))
     ROTATION = 0
 
-    def __init__(self, x, y, all_sprites):
+    def __init__(self, x, y):
         self.image = Pacman.image_1
         self.Num_Image = 0
         super().__init__(player_group, all_sprites)
@@ -72,7 +91,8 @@ class Pacman(pygame.sprite.Sprite):
         self.rect.x += x
         self.rect.y += y
 
-        self.Num_Image = (self.Num_Image + 1) % 4  # открывание и закрывание рта пакмэна
+        self.Num_Image = (self.Num_Image + 1) % 4
+        # открывание и закрывание рта пакмэна
         im = [
             Pacman.image_1,
             Pacman.image_eat,
@@ -84,7 +104,7 @@ class Pacman(pygame.sprite.Sprite):
 
         self.image = pygame.transform.rotate(tempImage, self.ROTATION)
 
-    def check_babah(self, lst_sprites):
+    def check_collision(self, lst_sprites):
         a = []
         for II, b in enumerate(lst_sprites):
             # if II == 0:
@@ -100,19 +120,19 @@ class Pacman(pygame.sprite.Sprite):
                 BXE = b.x + b.width + trace[2]
                 BYS = b.y + trace[3]
                 BYE = b.y + b.height + + trace[3]
-                checkPacmenLeft = (PXS >= BXS and PXS <= BXE)
-                checkPacmenRight = (PXE >= BXS and PXE <= BXE)
-                checkPacmenUP = (PYS >= BYS and PYS <= BYE)
-                checkPacmenDown = (PYE >= BYS and PYE <= BYE)
+                checkPacmanLeft = (PXS >= BXS and PXS <= BXE)
+                checkPacmanRight = (PXE >= BXS and PXE <= BXE)
+                checkPacmanUP = (PYS >= BYS and PYS <= BYE)
+                checkPacmanDown = (PYE >= BYS and PYE <= BYE)
 
-                return (checkPacmenLeft or checkPacmenRight) and (checkPacmenUP or checkPacmenDown)
+                return (checkPacmanLeft or checkPacmanRight) and (checkPacmanUP or checkPacmanDown)
 
             a.append(check(self.rect, b, [trace[0], trace[1], 0, 0]) or check(b, self.rect, [0, 0, trace[0], trace[1]]))
         b = any(a)
         return b
 
     def update(self):
-        a = horizontal_borders.sprites()[1].rect
+        a = tiles_group.sprites()[1].rect
         selfPoz = self.rect
         raz = selfPoz.x + selfPoz.width - a.x
         # print(raz)
@@ -120,32 +140,18 @@ class Pacman(pygame.sprite.Sprite):
         # tempself=copy.deepcopy(self)
         # tempself.move(trace[0], trace[1])
         # if pygame.sprite.spritecollideany(self, horizontal_borders):
-        if self.check_babah(list(horizontal_borders.sprites())):
+        if self.check_collision(list(tiles_group.sprites())):
             # print(f"корд пак: {selfPoz} | корд ст: {a}")
             self.move(0, 0)
             self.eating = False
             return True
-        # elif pygame.sprite.spritecollideany(self, vertical_borders):
-        #     self.move(0, 0)
-        #     self.eating = False
-        #     return True
-        # else:
-        #     self.move(trace[0], trace[1])
-        #     self.eating = True
-        elif self.check_babah(list(vertical_borders.sprites())):
-            self.move(0, 0)
-            self.eating = False
-            return True
-            # elif pygame.sprite.spritecollideany(self, horizontal_borders):
-            #     self.move(0, 0)
-            #     self.eating = False
-            #     return True
+
         else:
             self.move(trace[0], trace[1])
             self.eating = True
 
     def spawn(self, x, y):
-        self.rect = pygame.Rect(x, y, 50, 50)
+        self.rect = pygame.Rect(x, y, 20, 20)
 
     def rotate_UP(self):
         # self.image = pygame.transform.rotate(Pacman.image_1, 90)
@@ -171,21 +177,10 @@ class Pacman(pygame.sprite.Sprite):
 
 class Border(pygame.sprite.Sprite):
     def __init__(self, x1, y1, x2, y2):
-        # super().__init__(all_sprites)
-        # self.add(vertical_borders)
-        # self.image = pygame.Surface([50, 10])
-        # self.image.fill((125, 125, 125))
-        # self.rect = pygame.Rect(x1, y1, 50, 10)
         super().__init__(all_sprites)
-        # if x1 == x2:  # вертикальная стенка
-        #     self.add(vertical_borders)
-        #     self.image = pygame.Surface([100, 10])
-        #     self.image.fill((125, 125, 125))
-        #     self.rect = pygame.Rect(x1, y1, 100, 10)
-        # else:  # горизонтальная стенка
         self.add(horizontal_borders)
         self.image = pygame.Surface([x2, y2])
-        self.image.fill((125, 125, 125))
+        self.image.fill((255, 0, 0))
         self.rect = pygame.Rect(x1, y1, x2, y2)
 
 
@@ -201,50 +196,32 @@ if __name__ == '__main__':
     horizontal_borders = pygame.sprite.Group()
     vertical_borders = pygame.sprite.Group()
     clock = pygame.time.Clock()
-    tetric = '0'
-    b = False
 
-    Border(0, 0, WINDOW_WIDTH, 15)
-    Border(0, 0, 15, WINDOW_HEIGHT)
-    Border(0, WINDOW_WIDTH - 15, WINDOW_WIDTH, 15)
-    Border(WINDOW_HEIGHT - 15, 0, 15, WINDOW_HEIGHT)
-    Border(WINDOW_HEIGHT - 15, 0, 15, WINDOW_HEIGHT)
+    player, level_x, level_y = generate_level(load_level('level.txt'))
 
-    trace = (10, 0)
+    trace = (5, 0)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == TIMER_EVENT_TYPE:
-                tetric.forcedImageChange()
-                # tetric.(trace[0], trace[1])
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 3:
-                    if not b:
-                        tetric = Pacman(event.pos[0], event.pos[1], all_sprites)
-                        b = not b
-                    else:
-                        tetric.spawn(event.pos[0], event.pos[1])
+                player.forcedImageChange()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    tetric.rotate_RIGHT()
+                    player.rotate_RIGHT()
                     trace = (5, 0)
-                    # tetric.move(trace[0], trace[1])
                 if event.key == pygame.K_LEFT:
-                    tetric.rotate_LEFT()
+                    player.rotate_LEFT()
                     trace = (- 5, 0)
-                    # tetric.move(trace[0], trace[1])
 
                 if event.key == pygame.K_DOWN:
                     trace = (0, 5)
-                    # tetric.move(trace[0], trace[1])
-                    tetric.rotate_DOWN()
+                    player.rotate_DOWN()
 
                 if event.key == pygame.K_UP:
                     trace = (0, - 5)
-                    # tetric.move(trace[0], trace[1])
-                    tetric.rotate_UP()
+                    player.rotate_UP()
                 if event.key == pygame.K_SPACE:
                     all_sprites.update()
         all_sprites.update()
